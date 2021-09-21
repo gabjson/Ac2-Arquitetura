737eeb98038c104cc3c228be4189afcd4bdb7662
gabjson <69824323+gabjson@users.noreply.github.com>
2021-09-21 19:10:21 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -2,14 +2,14 @@ import abc
 from unittest import TestCase, main
 
 class Calculadora(object):
-    def calcular(self, num1, num2, operador):
+    def calcular(self, valor1, valor2, operador):
         operacaoFabrica = OperacaoFabrica()
         operacao = operacaoFabrica.criar(operador)
         if (operacao == None):
             return 0
         else:
-            result = operacao.executar(num1, num2)
-            return result
+            resultado = operacao.executar(valor1, valor2)
+            return resultado
 
 class OperacaoFabrica(object):
     def criar(self, operador):
@@ -24,30 +24,35 @@ class OperacaoFabrica(object):
 
 class Operacao(metaclass = abc.ABCMeta):
     @abc.abstractmethod
-    def executar(self, num1, num2):
+    def executar(self, valor1, valor2):
         pass
 
 class Soma(Operacao):
-    def executar(self, num1, num2):
-        result = num1 + num2
-        return result
+    def executar(self, valor1, valor2):
+        resultado = valor1 + valor2
+        return resultado
 
 class Subtracao(Operacao):
-    def executar(self, num1, num2):
-        result = num1 - num2
-        return result
+    def executar(self, valor1, valor2):
+        resultado = valor1 - valor2
+        return resultado
 
 class Multiplicacao(Operacao):
-    def executar(self, num1, num2):
-        result = num1 * num2
-        return result
+    def executar(self, valor1, valor2):
+        resultado = valor1 * valor2
+        return resultado
 
 class Divisao(Operacao):
-    def executar(self, num1, num2):
-        result = num1 / num2
-        return result
+    def executar(self, valor1, valor2):
+        resultado = valor1 / valor2
+        return resultado
 
 class Testes(TestCase):
+    def test_soma(self):
+        calc = Calculadora()
+        result = calc.calcular(10,10, 'soma')
+        self.assertEqual(result, 20)
+
     def test_multiplicacao(self):
         calc = Calculadora()
         result = calc.calcular(10,50, 'multiplicacao')
@@ -58,15 +63,10 @@ class Testes(TestCase):
         result = calc.calcular(100,10, 'divisao')
         self.assertEqual(result, 10)
 
-    def test_soma(self):
-        calc = Calculadora()
-        result = calc.calcular(10,10, 'soma')
-        self.assertEqual(result, 20)
-
     def test_subtracao(self):
         calc = Calculadora()
         result = calc.calcular(10,10, 'subtracao')
         self.assertEqual(result, 0)
-    
-if _name_ == '_main_':
+
+if name == 'main':
     main()
